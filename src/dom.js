eb3b0bb4927da2d48f952759e98e420dad4ddbaa
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-08-29 02:07:21 +0200

    Added comments

--- src/dom.js
+++ src/dom.js
@@ -13,6 +13,13 @@ import {
 	deleteProject,
 } from './buttons';
 
+// Assign Dataset to Project Card
+function assignProjectDataIndex(index, card, deleteBtn) {
+	card.dataset.project = index;
+	deleteBtn.dataset.deleteProjectBtn = index;
+}
+
+// Create a Project Card
 function createProjectCard(project) {
 	const index = document.getElementById('project-list').children.length - 1;
 	const card = createDomElement('div', 'project-card');
@@ -28,12 +35,45 @@ function createProjectCard(project) {
 	return card;
 }
 
+// Appends Project Card to Project List
 function appendProject(project) {
 	const list = document.getElementById('project-list');
 	const card = createProjectCard(project);
 	list.appendChild(card);
 }
 
+// Appends all Project Card to Project List
+function appendAllProjects(project) {
+	projects.forEach((project) => appendProject(project));
+}
+
+// Renders Projects
+function renderProjects(projects) {
+	resetElements('.project-card');
+	appendAllProjects();
+}
+
+// Renders the active projects tasks
+function renderActiveProject(project) {
+	setActiveProject(project);
+	selectProject(project);
+	renderTasks(project.tasks);
+}
+
+// Renders the active projects
+function selectProject(project) {
+	const projectTitle = document.getElementById('project-title');
+	projectTitle.textContent = project.title;
+}
+
+// Assigns dataset to task card
+function assignTaskDataIndex(index, card, doneBtn, deleteBtn) {
+	card.dataset.task = index;
+	doneBtn.dataset.doneBtn = index;
+	deleteBtn.dataset.deleteBtn = index;
+}
+
+// Create a new Task Card
 function createTaskCard(task) {
 	const index = document.getElementById('task-list').children.length - 1;
 	const card = createDomElement('div', 'task-card');
@@ -49,35 +89,25 @@ function createTaskCard(task) {
 	return card;
 }
 
-function renderActiveProject(project) {
-	setActiveProject(project);
-	selectProject(project);
-	renderTasks(project.tasks);
-}
-
-function renderProjects(projects) {
-	resetElements('.project-card');
-	appendAllProjects();
-}
-
+// Append a task card
 function appendTask(task) {
 	const list = document.getElementById('task-list');
 	const card = createTaskCard(task);
 	list.appendChild(card);
 }
 
+// Append all task cards
+function appendAllTasks(tasks) {
+	tasks.forEach((task) => appendTask(task));
+}
+
+// Renders Projects
 function renderTasks(tasks) {
 	resetElements('.task-card');
 	appendAllTasks(tasks);
 }
 
-function appendAllProjects(project) {
-	projects.forEach((project) => appendProject(project));
-}
-function appendAllTasks(tasks) {
-	tasks.forEach((task) => appendTask(task));
-}
-
+// Renders Task Details on details panel
 function renderTaskDetails(task) {
 	const title = document.getElementById('details-title');
 	const description = document.getElementById('details-description');
@@ -89,37 +119,25 @@ function renderTaskDetails(task) {
 	priority.textContent = task.priority ? `Priority: ${task.priority}` : '';
 }
 
-function assignTaskDataIndex(index, card, doneBtn, deleteBtn) {
-	card.dataset.task = index;
-	doneBtn.dataset.doneBtn = index;
-	deleteBtn.dataset.deleteBtn = index;
-}
-
-function assignProjectDataIndex(index, card, deleteBtn) {
-	card.dataset.project = index;
-	deleteBtn.dataset.deleteProjectBtn = index;
-}
-
+// Appends multiple elements to a div
 function appendToParent(parent, ...args) {
 	args.forEach((arg) => parent.appendChild(arg));
 }
 
+// Create a DOM element with relative class
 function createDomElement(type, elClass) {
 	const element = document.createElement(`${type}`);
 	element.classList.add(`${elClass}`);
 	return element;
 }
 
+// Resets all elements with a certain class
 function resetElements(elClass) {
 	let elements = document.querySelectorAll(elClass);
 	elements.forEach((element) => element.remove());
 }
 
-function selectProject(project) {
-	const projectTitle = document.getElementById('project-title');
-	projectTitle.textContent = project.title;
-}
-
+// Resets text input
 function resetTextInput(...args) {
 	args.forEach((arg) => (arg.value = ''));
 }
