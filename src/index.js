26723b64e7c32f15451746cc0f15cb5e0ac141be
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-09-06 02:05:00 +0200

    instructions added

--- src/index.js
+++ src/index.js
@@ -28,8 +28,64 @@ for (let i = 0; i < projects.length; i++) {
 
 // Creates default "Personal" project if projects array is empty when loading app
 if (projects[0] === undefined) {
+	newProject('Instructions');
 	newProject('Personal');
-	newProject('Work');
+	projects[0].addTask(
+		createTask(
+			'Projects',
+			'You can create new projects and edit their names from the Projects Panel',
+			undefined,
+			'0',
+			false
+		)
+	);
+	projects[0].addTask(
+		createTask(
+			'Tasks',
+			'You can create tasks for each project to stay more organized',
+			undefined,
+			'0',
+			false
+		)
+	);
+	projects[0].addTask(
+		createTask(
+			'Details',
+			'In the panel from which you are currently reading you can view all the details of a selected task and also edit it.',
+			undefined,
+			'0',
+			false
+		)
+	);
+	projects[0].addTask(
+		createTask(
+			'Task details',
+			'Every task can have a description a priority and a due date but only the title is always required',
+			undefined,
+			'0',
+			false
+		)
+	);
+	projects[0].addTask(
+		createTask(
+			'Complete and delete tasks',
+			'You can also set a task to done or completely remove a task by clicking the buttons on the relative task.',
+			undefined,
+			'0',
+			false
+		)
+	);
+	projects[0].addTask(
+		createTask(
+			'Delete instructions',
+			'You are all set. Go ahead and delete the instructions project and start using the todo list!',
+			undefined,
+			'0',
+			false
+		)
+	);
+	save(projects);
+	renderTasks(projects[0].tasks);
 }
 
 // Sets Active Project to the first one in projects array
